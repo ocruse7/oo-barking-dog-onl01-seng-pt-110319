178411efd86508fa2d48652a52135c35c1bbc200
Owen Cruse <ocruse7@gmail.com>
2019-12-19 18:49:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Dog
+  
+  def name=(dog_name)
+    @thisDog = dog_name
+  end
+  
+  def name
+    @thisDog
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
+  
+end
